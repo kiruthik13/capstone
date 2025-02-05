e319d37b19919496bc4ac0a66982997d96f4e34c
kiruthik13 <125745097+kiruthik13@users.noreply.github.com>
2025-02-05 11:10:55 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
     echo hi123
-    sh 'chmod +x build.sh'
-    sh './build.sh'
+    docker build -t test .
     docker login -u kiruthik1304 -p changeme
     docker tag test kiruthik1304/docker_jenkins_task2
     docker push kiruthik1304/docker_jenkins_task2
